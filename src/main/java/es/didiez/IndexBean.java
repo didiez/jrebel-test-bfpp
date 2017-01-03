caafa2914776a4bb752489ec58ca23b967c8f967
didiez <diegodiez.ddr@gmail.com>
2017-01-03 11:28:15 +0100

    Changes to reproduce a problem in jrebel reloaded bean with a customized scope set by a BeanFactoryPostProcessor. The reloaded bean is created as a 'singleton' when should be 'request' scoped.

--- src/main/java/es/didiez/IndexBean.java
+++ src/main/java/es/didiez/IndexBean.java
@@ -1,7 +1,6 @@
 package es.didiez;
 
 import static es.didiez.IndexBean.BEAN_NAME;
-import java.util.stream.Stream;
 import javax.enterprise.context.RequestScoped;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.stereotype.Component;
@@ -15,15 +14,23 @@ import org.springframework.stereotype.Component;
 public class IndexBean {
 
     public static final String BEAN_NAME = "indexBean";
+    private final ConfigurableListableBeanFactory context;
+    private int count = 0;    
     
     public IndexBean(ConfigurableListableBeanFactory context) {
-        if(!"request".equals(context.getBeanDefinition(BEAN_NAME).getScope())){
-            throw new IllegalStateException(BEAN_NAME + " must be request scope");
-        }
+        System.out.println("IndexBean constructor");
+        this.context = context;
     }
     
     public String hello(){
+        System.out.println("SCOPE --> " + context.getBeanDefinition(BEAN_NAME).getScope());
+        if(!"request".equals(context.getBeanDefinition(BEAN_NAME).getScope())){
+            throw new IllegalStateException(BEAN_NAME + " must be request scope");
+        }
+        if(count++ != 0) {
+            throw new IllegalStateException(BEAN_NAME + " not being a real request scope bean (persistent state across requests)");
+        }
         return "Hello World!";
-    }   
+    }
     
 }
